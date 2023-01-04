7eb5ed710ccd3be3944f67057337d5d288af6ba2
Marcio Tomaz <marciotmaz@gmail.com>
2023-01-04 20:54:40 -0300

    Add: exercício

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function isNumber(value) {
     }
 }
 console.log(isNumber('200'));
+const button = document.querySelector('button');
+if (button) {
+    button.click();
+}
+button?.click(); // mesma coisa que o de cima mas mais moderno, ele só vai adicionar o click se for diferente de null ou undefined
+// =========================== Exercício
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === 'number') {
+        return Number(value);
+    }
+    else if (typeof value === 'string') {
+        return Number(value);
+    }
+    else {
+        throw ('Value deve ser um número ou uma string');
+    }
+}
+console.log(toNumber(5));
+console.log(toNumber('5'));
+console.log(toNumber({}));
